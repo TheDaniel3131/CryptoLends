62ae679bb69a054be1215bd8d802c5ee714b622a
khslaijikl <91548066+khslaijikl@users.noreply.github.com>
2024-08-15 00:37:01 +0800

    Add files via upload

--- cl_ui/src/supabase/query/lendingListInsert.tsx
+++ cl_ui/src/supabase/query/lendingListInsert.tsx
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 export async function lendingListInsert(
     address: string,
     amount: number,
-    smart_contract:string,
+    currency:string,
     duration:number,
     interest_rate:number
 ): Promise<boolean> {
@@ -17,7 +17,7 @@ export async function lendingListInsert(
     const { data, error } = await supabase.rpc('insert_lending_list', {
         address: address,
         amount: amount,
-        contract: smart_contract,
+        currency: currency,
         duration_month: duration,
         rate: interest_rate
     });

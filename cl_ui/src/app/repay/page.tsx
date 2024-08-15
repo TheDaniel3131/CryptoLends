5f40a0f954f27ec01f61fda10cc750c997e19e5b
tp056310 <tp056310@mail.apu.edu.my>
2024-08-15 22:47:58 +0800

    my profile update

--- cl_ui/src/app/repay/page.tsx
+++ cl_ui/src/app/repay/page.tsx
@@ -1,6 +1,6 @@
 // import { Footer } from '@/components/Footer';
 // import LendingPage from '../../components/(repay)/Repayment';
-// import { Header } from '@/components/Header';
+// import { Header } from '@/components/Header'; //
 
 // export default function Lending() {
 //     return (
